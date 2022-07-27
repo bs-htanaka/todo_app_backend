276bca9380ccaa09952413dacf6394da7b5401c9
bs-htanaka <h.tanaka@buysell-technologies.com>
2022-07-28 01:37:21 +0900

    FEと疎通できるように設定追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,11 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i[ show edit update destroy ]
+  protect_from_forgery
 
   # GET /todos or /todos.json
   def index
     @todos = Todo.all
+    render json: @todos
   end
 
   # GET /todos/1 or /todos/1.json
@@ -22,39 +24,22 @@ class TodosController < ApplicationController
   # POST /todos or /todos.json
   def create
     @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo.save
+    render json: @todos
   end
 
   # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully updated." }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    set_todo
+    @todo.update(todo_params)
+    render json: @todos
   end
 
   # DELETE /todos/1 or /todos/1.json
   def destroy
     @todo.destroy
 
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    render json: @todos
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module TodoBackend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.permitted_origin = ENV.fetch('PERMITTED_ORIGIN', 'http://localhost:8080')
   end
 end
