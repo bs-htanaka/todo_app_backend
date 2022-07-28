5bcaa96a4ec32370aec612b3e1adfe98ead27a27
bs-htanaka <h.tanaka@buysell-technologies.com>
2022-07-29 03:15:54 +0900

    不要なもの削除

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,8 +2,6 @@ class SearchesController < ApplicationController
   protect_from_forgery
 
   def search
-    puts params[:keyword]
-    
     keyword = params[:keyword]
     @todos = Todo.where("name LIKE?","%#{keyword}%")
     render json: @todos
